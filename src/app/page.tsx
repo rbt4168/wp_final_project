4ccd6e935abe46d7800859674b4289dfdf98a6de
Paul2k3 <dreamandlife91@gmail.com>
2023-12-28 00:06:21 +0800

    get the data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import DivLineCenter from "@/components/divline";
 import FooterComponent from "@/components/footer";
 import NavigationBar from "@/components/navbar";
 import { main_theme } from "@/lib/themes";
-import { CssBaseline, Grid, Paper, TextField, ThemeProvider } from "@mui/material";
-
+import { CssBaseline, Grid, Paper, TextField, ThemeProvider, Button } from "@mui/material";
+import axios from "axios"
 export default function Home() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // TODO: sign in logic
+    axios.post("/api/userprofile", {}).then().catch(() => alert("你到底做什麼 解釋一下"))
+    
+  };
   return (
     <ThemeProvider theme={main_theme}>
       <CssBaseline/>
@@ -40,6 +46,16 @@ export default function Home() {
       
 
       <FooterComponent/>
+      <form onSubmit={handleSubmit}>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          修改資料 
+        </Button>
+      </form>
     </ThemeProvider>
   )
 }
---
+++ src/lib/auth/index.ts
@@ -0,0 +1,92 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+import { eq, and } from "drizzle-orm";
+
+import { db } from "@/db";
+import { usersTable } from "@/db/schema";
+
+import CredentialsProvider from "./CredentialsProvider";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [GitHub, Google ,CredentialsProvider],
+  callbacks: {
+    async session({ session, token }) {
+      const email = token.email || session?.user?.email;
+      const name = token.name || session?.user?.username;
+      console.log("```````````");
+      console.log(session?.user)
+      console.log("```````````");
+
+      if (!email) return session;
+      const [user] = await db
+        .select({
+          id: usersTable.displayId,
+          provider: usersTable.provider,
+          email: usersTable.email,
+          username: usersTable.username,
+        })
+        .from(usersTable)
+        .where(and(eq(usersTable.email, email.toLowerCase()), eq(usersTable.username, name as string)))
+        .execute();
+
+      return {
+        ...session,
+        user: {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          provider: user.provider,
+        },
+      };
+    },
+    async jwt({ token, account }) {
+      // Sign in with social account, e.g. GitHub, Google, etc.
+      if (!account) return token;
+      const { name, email } = token;
+      const provider = account.provider;
+      if (!name || !email || !provider) return token;
+      // Check if the email has been registered
+      if (provider === "github"){
+        const [existedGithubUser] = await db
+          .select({
+            id: usersTable.displayId,
+          })
+          .from(usersTable)
+          .where(and(eq(usersTable.email, email.toLowerCase()), eq(usersTable.provider, "github")))
+          .execute();
+        if (existedGithubUser){
+          console.log("why are you here")
+          return token;
+        }
+      }
+      else if (provider === "google"){
+        const [existedGoogleUser] = await db
+          .select({
+            id: usersTable.displayId,
+          })
+          .from(usersTable)
+          .where(and(eq(usersTable.email, email.toLowerCase()), eq(usersTable.provider, "google")))
+          .execute();
+        if (existedGoogleUser) return token;
+      }
+      if (!(provider === "github" || provider ==="google")) return token;
+
+      // Sign up
+      await db.insert(usersTable).values({
+        email: email.toLowerCase(),
+        username: name,
+        provider,
+      });
+
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+});
