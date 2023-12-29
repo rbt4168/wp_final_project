1e05d6ff7559f3d99dbae025962c37e177aab50d
Paul2k3 <dreamandlife91@gmail.com>
2023-12-30 00:06:19 +0800

    private

--- src/app/api/authorLowToHigh/route.ts
+++ src/app/api/authorLowToHigh/route.ts
@@ -1,26 +1,34 @@
 import { NextResponse } from "next/server";
 import { eq, asc } from "drizzle-orm";
 import { db } from "@/db";
-import { pictureTable } from "@/db/schema"; // Import your UserTable if not already done
+import { usersTable, pictureTable } from "@/db/schema"; // Import your UserTable if not already done
 
 export async function POST(request: Request) {
   try {
     // Authentication
     const body = await request.json();
     const {author_id} = body;
-
     // Query
+
+    const [author] = await db.select({
+      authorname: usersTable.username
+    })
+    .from(usersTable)
+    .where(eq(usersTable.id, author_id));
+    if (!author){
+      return new NextResponse("Internal Error", { status: 500 });
+    }
+    const prefix = `private-${author.authorname}`;
     const pictureLowToHigh = await db
       .select()
       .from(pictureTable)
       .where(eq(pictureTable.author_id, author_id))
       .orderBy(asc(pictureTable.pic_id))
       .execute();
-    const pictureIds = pictureLowToHigh.map(picture => picture.pic_id);
-    console.log("--------------==================");
-    console.log("Low")
-    console.log(pictureIds);
-    console.log("--------------==================");
+    const pictureIds = pictureLowToHigh.filter(picture =>
+    picture.tags && !picture.tags.some(tag => tag.startsWith(prefix))
+    ).map(picture => picture.pic_id);
+    
     // Return the user information
     return NextResponse.json({ pictureIds });
   } catch (error) {
--- src/app/api/authorTag/route.ts
+++ src/app/api/authorTag/route.ts
@@ -1,68 +1,65 @@
 import { NextResponse } from "next/server";
-import { eq } from "drizzle-orm";
+import { eq, desc, inArray } from "drizzle-orm";
 import { db } from "@/db";
-import { auth } from "@/lib/auth";
 import { pictureTable, usersTable } from "@/db/schema"; // Import your UserTable if not already done
+import { auth } from "@/lib/auth";
 
 export async function POST(request: Request) {
   try {
     // Authentication
+    const body = await request.json();
+    const {author_id} = body;
+    // Query
     const session = await auth();
     if (!session?.user?.email) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
     // Extract data from request body
-    const body = await request.json();
-    console.log(body.pic_id);
+   
 
     // Assume you have a user table where you want to update this information
     // and 'userId' is obtained from the session or some other source
     const session_id = session?.user?.id; // Replace with actual way to get the user's ID
 
-    const User = await db
+    const [User] = await db
           .select({
             author: usersTable.name,
-            id   : usersTable.id
+            id   : usersTable.id,
+            owned_private_tag : usersTable.owned_private_tag,
           })
           .from(usersTable)
           .where(eq(usersTable.displayId, session_id))
           .execute();
-    
     // Update user profile in the database
     if (!User){
         // console.log("fefefe");
         return new NextResponse("No author you bad guy", { status: 401 });
     }
     
-    await db.delete(pictureTable)
-    .where(eq(pictureTable.pic_id, body.pic_id))
-    .returning();
-    
-    const [currentUser] = await db
-      .select()
-      .from(usersTable)
-      .where(eq(usersTable.username,(session?.user?.username)));
-    
-    if (!currentUser.post_picture){
-        return new NextResponse("No author you bad guy", { status: 401 });
-    }
-    // Check if the user has an existing post_picture array, if not, initialize it
-    const currentPostPictures = currentUser.post_picture || [];
-// Remove the specified pic_id from the array
-    const updatedPostPictures = currentPostPictures.filter(id => id !== body.pic_id);
-    
-    // Update the user's record
-    await db
-      .update(usersTable)
-      .set({
-        post_picture: updatedPostPictures,
-      })
-      .where(eq(usersTable.username, session?.user?.username))
-      .returning();
 
-    // Return the updated user information
-    return NextResponse.json({ body });
+    const pictures = await db
+      .select({
+        pic_id: pictureTable.pic_id,
+        tags: pictureTable.tags
+      
+      })
+      .from(pictureTable)
+      .where(eq(pictureTable.pic_id, author_id))
+      .orderBy(desc(pictureTable.pic_id))
+      .execute();
+      if (!User.owned_private_tag || User.owned_private_tag.length === 0) {
+        return NextResponse.json("No picture", { status: 404 });
+      }
+    // Return the user information
+    const matchedPictureIDs = pictures.filter(picture =>
+      picture.tags && picture.tags.some(tag => User.owned_private_tag && User.owned_private_tag.includes(tag))
+    ).map(picture => picture.pic_id);
+    
+    console.log("match");
+    console.log(matchedPictureIDs);
+    console.log("match");
+    return NextResponse.json({ matchedPictureIDs });
   } catch (error) {
     console.error("Error in POST function: ", error);
     return new NextResponse("Internal Error", { status: 500 });
