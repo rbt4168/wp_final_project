2494fb83515b9f44a4f381ad96322be59a88bbe2
rbt4168 <rbt4168@gmail.com>
2023-12-29 20:17:13 +0800

    style: bcoin

--- src/app/buy_coin/page.tsx
+++ src/app/buy_coin/page.tsx
@@ -1,29 +1,22 @@
 "use client"
-import * as React from 'react';
+
+import { useState } from 'react';
+import { Box, Button, Card, CardActions, CardContent, 
+  CardHeader, Container, CssBaseline, Grid, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import StarIcon from '@mui/icons-material/StarBorder';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 
 import NavigationBar from '@/components/navbar';
-import { main_theme } from '@/lib/themes';
 import FooterComponent from '@/components/footer';
+
+import { main_theme } from '@/lib/themes';
+
 import axios from "axios"
-import { useState } from 'react';
 
 const tiers = [
   {
     title: '少量 Coin',
     subheader: '',
-    price: '10',
+    price: 10,
     description: [
       '向創作投以小小支持',
       '享有對方的感謝',
@@ -35,7 +28,7 @@ const tiers = [
   {
     title: '一堆 Coin',
     subheader: 'Most popular',
-    price: '100',
+    price: 100,
     description: [
       '更多的 Coin',
       '讓創作者更積極地創作',
@@ -46,7 +39,7 @@ const tiers = [
   },
   {
     title: '好多 Coin',
-    price: '300',
+    price: 300,
     description: [
       '有助於更多、更優質的內容',
       '與創作者和其他支持者交流',
@@ -60,25 +53,22 @@ const tiers = [
 export default function Pricing() {
   const [isDisabled, setIsDisabled] = useState(false);
 
-  const handleSubmit = (e : any) => {
-    // Disable all buttons
+  function handleSubmit(amount:number){
     setIsDisabled(true);
 
     const payload = {
-      coins: Number(e)
+      coins: amount
     };
 
-    // Make the API call
     axios.post("/api/buy_coin", payload)
       .then(response => {
         alert("儲值成功");
       }).catch((e) => {
         alert("Error occurred while paying");
       }).finally(() => {
-        // Re-enable all buttons, whether the call was successful or failed
         setIsDisabled(false);
       });
-  };
+  }
 
   return (
     <ThemeProvider theme={main_theme}>
@@ -103,7 +93,6 @@ export default function Pricing() {
       <Container maxWidth="md" component="main">
         <Grid container spacing={5} alignItems="flex-end">
           {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
             <Grid
               item
               key={tier.title}
@@ -116,12 +105,11 @@ export default function Pricing() {
                   title={tier.title}
                   subheader={tier.subheader}
                   titleTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
                   subheaderTypographyProps={{
                     align: 'center',
                   }}
                   sx={{
-                    backgroundColor: (theme) =>
+                    backgroundColor: (theme:any) =>
                       theme.palette.mode === 'light'
                         ? theme.palette.secondary.main
                         : theme.palette.grey[700],
