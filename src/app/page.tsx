82cb9b75e337f32dfac7a18d7a784a38209df681
rbt4168 <rbt4168@gmail.com>
2023-12-29 17:30:07 +0800

    feat: merge fo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,44 +4,46 @@ import FooterComponent from "@/components/footer";
 import NavigationBar from "@/components/navbar";
 import { main_theme } from "@/lib/themes";
 
-import { Button, CssBaseline, Grid, TextField, ThemeProvider } from "@mui/material";
+import { CssBaseline, Grid, ThemeProvider } from "@mui/material";
 
 import { useState, useEffect } from "react";
 import { WorkCardComponent } from "@/components/workcomponent";
-import axios from "axios"
 import SearchBar from "@/components/searchbarcom";
+
+import axios from "axios"
 export default function Home() {
-  const [ searchText, setSearchText ] = useState("");
+  // const [ searchText, setSearchText ] = useState("");
 
   const [ foArr, setFoArr ] = useState([0,0,0,0,0]);
   const [ hotArr, setHotArr ] = useState([0,0,0,0,0]);
   const [ recentArr, setRecentArr ] = useState([0,0,0,0,0]);
 
   useEffect(()=>{
-    axios.get("/api/homeFollowedLatest").then((e)=>{
-      console.log(e.data);
-    }).catch((e)=>console.error(e));
+    axios.get("/api/homeFollowedLatest").then((e:any)=>{
+      // console.log(e.data);
+      setFoArr(e.data.pictureIds);
+    }).catch((e:any)=>console.error(e));
 
-    axios.get("/api/homeRecent").then((e)=>{
+    axios.get("/api/homeRecent").then((e:any)=>{
       // console.log(e.data);
       setRecentArr(e.data.pictureIds);
-    }).catch((e)=>console.error(e));
+    }).catch((e:any)=>console.error(e));
 
-    axios.get("/api/homeHot").then((e)=>{
+    axios.get("/api/homeHot").then((e:any)=>{
       setHotArr(e.data.pictureIds);
-    }).catch((e)=>console.error(e));
+    }).catch((e:any)=>console.error(e));
 
   }, []);
   
   const handleSubmit = () => {
     // TODO: search api
-    const payload = {
-      keyword: searchText,
-    }
-    alert(payload);
-    axios.post("/api/search", payload).then((e)=>{
-      setHotArr(e.data.pictureIds);
-    }).catch((e)=>console.error(e));
+    // const payload = {
+    //   keyword: searchText,
+    // }
+    // alert(payload);
+    // axios.post("/api/search", payload).then((e:any)=>{
+    //   setHotArr(e.data.pictureIds);
+    // }).catch((e:any)=>console.error(e));
   };
 
   return (
